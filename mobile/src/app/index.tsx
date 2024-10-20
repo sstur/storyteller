bd699d9961fcbb1a31df2225cbc3fcfa31479f7d
Simon <sstur@me.com>
2024-10-20 14:35:19 -0700

    Fetch and display stories

--- mobile/src/app/index.tsx
+++ mobile/src/app/index.tsx
@@ -1,12 +1,59 @@
-import { Home } from '@tamagui/lucide-icons';
+import { useQuery } from '@tanstack/react-query';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
-import { Button, Text, YStack } from '~/components/core';
+import { ScrollView, Spinner, Text } from '~/components/core';
+import { API_BASE_URL } from '~/support/constants';
 
-export default function App() {
+type Story = {
+  title: string;
+  description: string;
+  imagePrompt: string;
+};
+
+async function getStories() {
+  const response = await fetch(API_BASE_URL + '/story-ideas');
+  if (!response.ok) {
+    throw new Error(`Unexpected response status: ${response.status}`);
+  }
+  const data = await response.json();
+  // eslint-disable-next-line @typescript-eslint/consistent-type-assertions
+  return data as Array<Story>;
+}
+
+function StoryListContent() {
+  const { data, status, error } = useQuery({
+    queryKey: ['getStories'],
+    queryFn: getStories,
+  });
+
+  if (status === 'error') {
+    return <Text>{String(error)}</Text>;
+  }
+  if (status === 'pending') {
+    return <Spinner />;
+  }
   return (
-    <YStack flex={1} justifyContent="center" alignItems="center" gap="$4">
-      <Button icon={<Home />}>{t('Hello world!')}</Button>
-      <Text>{t('Hello world!')}</Text>
-    </YStack>
+    <>
+      {data.map((story, i) => {
+        return <Text key={i}>{story.title}</Text>;
+      })}
+    </>
+  );
+}
+
+export default function StoryList() {
+  const safeAreaInsets = useSafeAreaInsets();
+  return (
+    <ScrollView
+      flex={1}
+      contentContainerStyle={{
+        paddingTop: safeAreaInsets.top,
+        paddingBottom: safeAreaInsets.bottom,
+        paddingHorizontal: '$3',
+        gap: '$3',
+      }}
+    >
+      <StoryListContent />
+    </ScrollView>
   );
 }
