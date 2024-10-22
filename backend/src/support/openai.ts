61f36d1eff73c9163017ce4ee3549b161e731489
Simon <sstur@me.com>
2024-10-22 09:12:27 -0700

    Remove extraneous openai config options

--- backend/src/support/openai.ts
+++ backend/src/support/openai.ts
@@ -4,6 +4,4 @@ import { OPENAI_API_KEY } from '~/support/constants';
 
 export const openai = new OpenAI({
   apiKey: OPENAI_API_KEY,
-  organization: 'org-6CfBKThygFpUJ8lrfQM8zReY',
-  project: 'proj_BIqjzbCztprpxB4F8RU58NN9',
 });
