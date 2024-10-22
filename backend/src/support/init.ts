41b83066c38f66c443df9656b17af0be9608bb40
Simon <sstur@me.com>
2024-10-21 22:10:59 -0700

    Update init -> dotenv

--- backend/src/support/init.ts
+++ backend/src/support/init.ts
@@ -1,1 +1,6 @@
-import 'dotenv/config';
+/* eslint-disable @typescript-eslint/no-var-requires, @typescript-eslint/no-unsafe-call */
+
+if (process.env.NODE_ENV !== 'production') {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+  require('dotenv').config();
+}
