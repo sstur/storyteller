bab77c1139056dd6cd6cbc3c78b16ed6481f1435
Simon <sstur@me.com>
2024-11-25 09:20:06 -0800

    Add Pothos and yoga-graphql

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,17 +2,30 @@ import '~/support/dotenv';
 
 import { createServer } from 'http';
 import { Readable } from 'stream';
+import type { ReadableStream } from 'stream/web';
+
+import { createYoga } from 'graphql-yoga';
 
 import { handleRequest } from '~/handleRequest';
 import { HttpError } from '~/support/HttpError';
 import { parseIncomingMessage } from '~/support/parseIncomingMessage';
 
+import { schema } from './graphql/graphql-schema';
+
 const PORT = 8000;
+
+const yoga = createYoga({
+  schema,
+});
 
 const server = createServer();
 
 server.on('request', (req, res) => {
   const [url, request] = parseIncomingMessage(req);
+  if (url.pathname === '/graphql') {
+    void yoga(req, res);
+    return;
+  }
   void handleRequest(url.pathname, request)
     .catch((error: unknown) => {
       if (error instanceof HttpError) {
@@ -31,7 +44,8 @@ server.on('request', (req, res) => {
         res.setHeader(name, value);
       }
       if (body) {
-        Readable.fromWeb(body).pipe(res);
+        // eslint-disable-next-line @typescript-eslint/consistent-type-assertions
+        Readable.fromWeb(body as ReadableStream).pipe(res);
       } else {
         res.end();
       }
